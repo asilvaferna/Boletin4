ddb3e78c877902c76d72b76cf2de751bbd188501
asilvaferna <blackhatnairda@gmail.com>
2016-10-19 08:41:35 +0200

    Añdidos comentarios

--- Boletin4_2/src/boletin4_2/Boletin4_2.java
+++ Boletin4_2/src/boletin4_2/Boletin4_2.java
@@ -1,8 +1,8 @@
 package boletin4_2;
 public class Boletin4_2 {
     public static void main(String[] args) {
-        Satelite sat1 = new Satelite(100, 20, 30);
-        sat1.verPosicion();
+        Satelite sat1 = new Satelite(100, 20, 30); //Creamos un objeto de tipo "Satelite" y le damos valores
+        sat1.verPosicion(); //Vemos la posicion en la que está el satelite
        
     }
     
