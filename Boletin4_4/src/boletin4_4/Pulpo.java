5eef18e1c64964bb0f969c4167de0188f5c1ad66
asilvaferna <blackhatnairda@gmail.com>
2016-10-19 09:08:32 +0200

    Añadidos comentarios

--- Boletin4_4/src/boletin4_4/Pulpo.java
+++ Boletin4_4/src/boletin4_4/Pulpo.java
@@ -1,7 +1,8 @@
 package boletin4_4;
 public class Pulpo {
-    private int pulpo, patatas;
-    public Pulpo(){
+    private int pulpo, patatas; //Declaramos las variables y las encapsulamos
+    //Metodos constructores que inicializan los valores de los atributos
+    public Pulpo(){ 
         pulpo = 0;
         patatas = 0;
     }
@@ -9,20 +10,20 @@ public class Pulpo {
         pulpo = pu;
         patatas = pa;
     }
-    public void numClientes(int pu, int pa){
+    public void numClientes(int pu, int pa){ //Creamos un metodo que devuelve el numero de comensales posibles
         if (pu / 2 > pa) System.out.println(3 * pu / 2);
         else System.out.println(3 * pa);
     }
-    public void setPulpo(int x){
+    public void setPulpo(int x){ //Creamos un metodo que incrementa las unidades de pulpo
         pulpo += x;
     }
-    public void setPatatas(int x){
+    public void setPatatas(int x){ //Creamos un metodo que incrementa las unidades de patatas
         patatas += x;
     }
-    public void getPulpo(){
+    public void getPulpo(){ //Creamos un metodo que devuelve las unidades de pulpo
         System.out.println("La cantidad de pulpo es: " + pulpo + " Kg");
     }
-    public void getPatatas(){
+    public void getPatatas(){ //Creamos un metodo que devuelve las unidades de patatas
         System.out.println("La cantidad de patatas es: " + patatas + " Kg");
     }
 }
