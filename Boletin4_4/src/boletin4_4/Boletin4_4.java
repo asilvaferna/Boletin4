0d4ba76d7902998e78daf1b538f13e7ec8a19f17
asilvaferna <blackhatnairda@gmail.com>
2016-10-19 09:18:12 +0200

    Añadidos comentarios

--- Boletin4_4/src/boletin4_4/Boletin4_4.java
+++ Boletin4_4/src/boletin4_4/Boletin4_4.java
@@ -1,12 +1,12 @@
 package boletin4_4;
 public class Boletin4_4 {
     public static void main(String[] args) {
-        Pulpo p1 = new Pulpo(11,5);
-        p1.setPulpo(1);
-        p1.setPatatas(1);
-        p1.getPulpo();
-        p1.getPatatas();
-        p1.numClientes(12,6);
+        Pulpo p1 = new Pulpo(11,5); //Instanciamos un objeto de tipo "Pulpo"
+        p1.setPulpo(1); //LLamamos al metodo setPulpo y le damos valor 1
+        p1.setPatatas(1); //Llamamos al metodo setPatatas y le damos valor 1
+        p1.getPulpo(); //Llamamos al metodo getPulpo para obtener las unidades de Pulpo
+        p1.getPatatas(); //Llamamos al metodo getPatatas para obtener las unidades de Patatas
+        p1.numClientes(12,6); //Llamamos al metodo numClientes y le damos los valores 12 y 6 
 
     }
     
