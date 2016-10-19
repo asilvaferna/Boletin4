916c09d825906dfc6d7d60d7a921ee9be1a6d5e8
asilvaferna <blackhatnairda@gmail.com>
2016-10-19 08:50:23 +0200

    Añadidos comentarios

--- Boletin4_3/src/boletin4_3/Circulo.java
+++ Boletin4_3/src/boletin4_3/Circulo.java
@@ -1,17 +1,18 @@
 package boletin4_3;
 public class Circulo {
-    private double radio;
-    static final double PI = 3.14;
+    private double radio; //Declaramos variable y la encapsulamos
+    static final double PI = 3.14; //Declaramos una constante de tipo double
+    //Metodos constructores que inicializan los valores de los atributos
     public Circulo(){
         radio = 0;
     }
     public Circulo(double r){
         radio = r;
     }
-    public void calcularArea(){
+    public void calcularArea(){ //Metodo para calcular el area y devolverlo por pantalla
         System.out.println("El area es: " + PI * (Math.pow(radio, 2)));
     }
-    public void calcularLongitud(){
+    public void calcularLongitud(){ //Metodo para calcular la longitud y devolverlo por pantalla
         System.out.println ("La longitud es: " + 2 * PI * radio);
     }
     
