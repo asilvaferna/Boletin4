e6329c970282ca965b2ce3801d76d858e6072d78
asilvaferna <blackhatnairda@gmail.com>
2016-10-19 08:51:53 +0200

    Añadidos comentarios

--- Boletin4_3/src/boletin4_3/Boletin4_3.java
+++ Boletin4_3/src/boletin4_3/Boletin4_3.java
@@ -1,9 +1,9 @@
 package boletin4_3;
 public class Boletin4_3 {
-    public static void main(String[] args) {
-        Circulo circ1 = new Circulo(12.5);
-        circ1.calcularArea();
-        circ1.calcularLongitud();
+    public static void main(String[] args) { 
+        Circulo circ1 = new Circulo(12.5); //Creamos un objeto de tipo "Circulo" y le damos un valor
+        circ1.calcularArea(); //Llamamos al metodo calcularArea para calcular el area y devolverlo por pantalla
+        circ1.calcularLongitud(); //Llamamos al metodo calcularLongitud para calcular la longitud y devolverla por pantalla
 
     }
     
